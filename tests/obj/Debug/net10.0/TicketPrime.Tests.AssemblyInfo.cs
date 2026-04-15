2dde6d3cc719920b6eb50ef0dcf2a63c8763a4ff
Kauã Landi <eu@kaualf.com>
2026-04-15 01:29:48 -0300

    chore: update binary files and assembly information for TicketPrime.Tests

--- tests/obj/Debug/net10.0/TicketPrime.Tests.AssemblyInfo.cs
+++ tests/obj/Debug/net10.0/TicketPrime.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TicketPrime.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a580118e8c4da6b887dac16dbbd58450b123bf5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35883c70e6f6dde58d4405c96d10df7a7ecc3173")]
 [assembly: System.Reflection.AssemblyProductAttribute("TicketPrime.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TicketPrime.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
